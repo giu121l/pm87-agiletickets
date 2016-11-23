f2db7bef5d6bd1b3a81ba3b5f25ccb12ec7efe64
giuliano <giu121@gmail.com>
2016-11-23 16:58:24 -0200

    segunda refatoração

--- src/main/java/br/com/caelum/agiletickets/domain/precos/CalculadoraDePrecos.java
+++ src/main/java/br/com/caelum/agiletickets/domain/precos/CalculadoraDePrecos.java
@@ -11,8 +11,9 @@ public class CalculadoraDePrecos {
 		BigDecimal preco;
 		double totalIngressos = sessao.getTotalIngressos().doubleValue();
 		double ingressoReservados = sessao.getIngressosReservados();
+		TipoDeEspetaculo tipoEspetaculo = sessao.getEspetaculo().getTipo();
 		
-		if(sessao.getEspetaculo().getTipo().equals(TipoDeEspetaculo.CINEMA) || sessao.getEspetaculo().getTipo().equals(TipoDeEspetaculo.SHOW)) {
+		if(tipoEspetaculo.equals(TipoDeEspetaculo.CINEMA) || tipoEspetaculo.equals(TipoDeEspetaculo.SHOW)) {
 			//quando estiver acabando os ingressos... 
 			if((totalIngressos - ingressoReservados) / totalIngressos <= 0.05) { 
 				preco = sessao.getPreco().add(sessao.getPreco().multiply(BigDecimal.valueOf(0.10)));
@@ -29,7 +30,7 @@ public class CalculadoraDePrecos {
 			if(sessao.getDuracaoEmMinutos() > 60){
 				preco = preco.add(sessao.getPreco().multiply(BigDecimal.valueOf(0.10)));
 			}
-		} else if(sessao.getEspetaculo().getTipo().equals(TipoDeEspetaculo.ORQUESTRA)) {
+		} else if(tipoEspetaculo.equals(TipoDeEspetaculo.ORQUESTRA)) {
 			if((totalIngressos - ingressoReservados) / totalIngressos <= 0.50) { 
 				preco = sessao.getPreco().add(sessao.getPreco().multiply(BigDecimal.valueOf(0.20)));
 			} else {
